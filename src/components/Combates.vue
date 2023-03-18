df26a71ec0c8589f9419455b96a49d0567dc7b6d
GabriMac <gabri1998gabri@gmail.com>
2023-03-19 00:19:23 +0100

    base done

--- src/components/Combates.vue
+++ src/components/Combates.vue
@@ -1,5 +1,41 @@
 <template>
-Combates
+    <div class="firstBorder">
+        <div class="matchBorder">
+            <div class="border border-dark">
+
+                <div class="mt-2"></div>
+                <div class="singleMatchPadding">
+                    <div class="d-flex justify-content-between align-items-baseline singleMatch">
+                        <div>
+                            <img class="trainerImage" src="@/assets/trainerPixel.png"/> Avdalian
+                        </div>
+                        <div>
+                            VS
+                        </div>
+                        <div>
+                            Avdalian <img class="trainerImage" src="@/assets/trainerPixel.png"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="singleMatchPadding">
+                    <div class="d-flex justify-content-between align-items-baseline singleMatch">
+                        <div>
+                            <img class="trainerImage" src="@/assets/trainerPixel.png"/> Avdalian
+                        </div>
+                        <div>
+                            VS
+                        </div>
+                        <div>
+                            Avdalian <img class="trainerImage" src="@/assets/trainerPixel.png"/>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +44,38 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+.firstBorder{
+    border: solid;
+    border-width: 2px;
+    border-radius: 17px;
+    border-color: black;
+}
+
+.trainerImage{
+  width: 30px;
+}
+
+.singleMatch{
+    border: solid;
+    border-radius: 10px;
+    border-width: 1px;
+    border-color: black;
+    background-color: #F0F0F0;
+    padding-left: 5px;
+}
+
+.singleMatchPadding {
+    padding: 5px;
+}
+
+.matchBorder{
+    border: solid;
+    border-radius: 15px;
+    border-width: 15px;
+    border-color: #AAAAAA;
+    -moz-border-radius: 15px; 
+    -webkit-border-radius: 15px; 
+}
 </style>
