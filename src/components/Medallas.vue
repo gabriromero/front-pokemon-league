9e10c98749d8ae73a362058eea3e6c6efb4fa0b7
danielavdalian <avdaliansfdk@gmail.com>
2023-03-21 00:21:00 +0100

    Añadido sistema de medallas (oscurecidas hasta que se programe el debloqueo)

---
+++ src/components/Medallas.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="medallas panel-body col">
+    <div class="medalla">
+      <div class="medalla_2">
+        <img
+          v-if="ladoMedalla == 'L'"
+          src="@/assets/Medallas/Medalla_C3Ffiro.png"
+        />
+        <img v-else src="@/assets/Medallas/Medalla_Tormenta.png" />
+      </div>
+    </div>
+    <div class="medalla">
+        <div class="medalla_2">
+      <img
+        v-if="ladoMedalla == 'L'"
+        src="@/assets/Medallas/Medalla_Colmena.png"
+      />
+      <img v-else src="@/assets/Medallas/Medalla_Mineral.png" />
+    </div>
+</div>
+    <div class="medalla">
+        <div class="medalla_2">
+      <img
+        v-if="ladoMedalla == 'L'"
+        src="@/assets/Medallas/Medalla_Planicie.png"
+      />
+      <img v-else src="@/assets/Medallas/Medalla_Glaciar.png" />
+    </div>
+</div>
+    <div class="medalla">
+        <div class="medalla_2">
+      <img
+        v-if="ladoMedalla == 'L'"
+        src="@/assets/Medallas/Medalla_Niebla.png"
+      />
+      <img v-else src="@/assets/Medallas/Medalla_Drag3Fn.png" />
+    </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ladoMedalla: String,
+  },
+};
+</script>
+
+
+<style scoped>
+.medallas {
+  background-color: #d9d9d9;
+  border-radius: 15px;
+  height: 100%;
+  margin-left: 5%;
+  margin-right: 5%;
+  border: 2px solid;
+  border-color: black;
+
+}
+.medalla {
+  background-color: #888686;
+  border: 1px solid;
+  border-color: black;
+  border-radius: 15px;  
+  width: 100%;
+  max-width:100px;
+  height: 100px;
+  position: relative;
+  margin: 0 auto;
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+.medalla_2 {
+  background-color: #AAAAAA;
+  border: 1px solid;
+  border-color: black;
+  width: 75%;
+  border-radius: 15px;  
+  height: 75%;
+  position: absolute;
+  top: 12.5%;
+  left: 12.5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,13 +1,54 @@
 <template>
-  <h1>Perfil</h1>
+  <div class="mainCenter">
+    <p class="tituloFicha pl-title text-center border border-dark">
+      FICHA ENTRENADOR
+    </p>
+    <p class="nombreFicha pl-title text-center border border-dark">Guzzom</p>
+  </div>
+  <div class="row">
+    <div class="border border-5 col-sm-2">
+      <Medallas :ladoMedalla="'L'" />
+    </div>
+    <div class="border border-5 col-sm-8"></div>
+    <div class="border border-5 col-sm-2">
+      <Medallas :ladoMedalla="'R'"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import Medallas from "@/components/Medallas.vue";
 export default {
-
-}
+  components: { Medallas },
+};
 </script>
 
 <style>
-
+.tituloFicha {
+  border-radius: 20px;
+  width: 75%;
+  margin: 0 auto;
+  padding: 5x;
+  font-weight: bold;
+}
+.mainCenter {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+.medallas img {
+  max-width: 100%;
+  height: 40%;
+  max-height: 100%;
+  filter: grayscale(1);
+}
+.nombreFicha {
+  border-radius: 20px;
+  margin: 0 auto;
+  width: fit-content;
+  padding: 15px;
+  margin-top: 10px;
+}
+.row {
+}
 </style>
