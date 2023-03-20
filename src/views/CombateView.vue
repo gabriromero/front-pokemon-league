e16ef0128fcf103d2b88c48bda81867e5e05ec23
GabriMac <gabri1998gabri@gmail.com>
2023-03-20 03:10:35 +0100

    finished combate first version

--- src/views/CombateView.vue
+++ src/views/CombateView.vue
@@ -1,5 +1,105 @@
 <template>
-  <h1>Combate</h1>
+  <div id="combate" class="pt-2 container">
+    <div>
+      <h3>COMBATES PENDIENTES</h3>      
+      <div class="conjunto-combates">
+
+        <div class="mt-4"></div>
+        <div class="border-single-match">
+          <div class="single-match container-fluid">
+            <div class="row d-flex align-items-center">
+              <div class="col-lg-5 trainer-select">
+                <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/> Avdalian
+              </div>
+              <div class="col-lg-5 trainer-select ">
+                Avdalian <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/>
+              </div>
+              <div class="col-lg-2 handicap">
+                <div class="handicap-div">
+                  Sin handicap
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-4"></div>
+        <div class="border-single-match">
+          <div class="single-match container-fluid">
+            <div class="row d-flex align-items-center">
+              <div class="col-lg-5 trainer-select">
+                <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/> Avdalian
+              </div>
+              <div class="col-lg-5 trainer-select ">
+                Avdalian <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/>
+              </div>
+              <div class="col-lg-2 handicap">
+                <div class="handicap-div">
+                  Sin handicap
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-4"></div>
+      </div>
+    </div>
+    <div class="mt-4">
+      
+      <br><br>
+      <h3>COMBATES FINALIZADOS</h3>
+
+      <div class="conjunto-combates">
+        <div class="mt-4"></div>
+        <div class="border-single-match">
+          <div class="single-match container-fluid">
+            <div class="trainer-select-finished loser-result">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-4">
+                  <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/> Avdalian
+                </div>
+                <div class="col-lg-1">
+                  VS
+                </div>
+                <div class="col-lg-4">
+                  <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/> Avdalian
+                </div>
+                <div id="winner" class="col-lg-3 ">
+                  <h3>GANA GUZZOM</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-4"></div>
+        <div class="border-single-match">
+          <div class="single-match container-fluid">
+            <div class="trainer-select-finished winner-result">
+              <div class="row d-flex align-items-center">
+                <div class="col-lg-4">
+                  <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/> Avdalian
+                </div>
+                <div class="col-lg-1">
+                  VS
+                </div>
+                <div class="col-lg-4">
+                  <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false"/> Avdalian
+                </div>
+                <div id="winner" class="col-lg-3 ">
+                  <h3>GANA GUZZOM</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-4"></div>
+        </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -8,6 +108,93 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+#combate {
+  text-align: center;
+}
+
+
+
+.conjunto-combates{
+  border: solid #000000;
+  border-width: 1px;
+  border-radius: 15px;
+  background-color: #D0D0D0;
+}
+
+.border-single-match {
+  border: solid black;
+  border-radius: 15px;
+  margin: 15px;
+  border-width: 1px;
+}
+
+.single-match {
+  border: solid #AAAAAA;
+  border-radius: 15px;
+  border-width: 10px;
+  background-color: #AAAAAA;
+}
+
+.trainer-select {
+  border: solid black;
+  border-width: 1px;
+  background-color: #D9D9D9;
+}
+
+.trainer-select-finished {
+  border: solid black;
+  border-width: 1px;
+  background-color: #D9D9D9;
+  border-radius: 7px;
+}
+
+.trainer-select:hover {
+  background-color:#BDCAB7;
+  cursor: pointer;
+  -moz-user-select: none;
+   -khtml-user-select: none;
+   -webkit-user-select: none;
+   -ms-user-select: none;
+   user-select: none;
+}
+.handicap-div {
+  border: solid black;
+  border-width: 1px;
+  border-radius: 15px;
+  background-color: #D9D9D9;
+  font-size: 12px;
+  height: 96px;
+  margin: auto;
+  padding-top: 40px;
+}
+
+@media screen and (max-width: 992px){
+   .handicap-div{
+    height: 30px;
+    padding-top: 6px;
+    margin-top: 10px;
+   } 
+
+   #winner {
+    margin-bottom: 20px;
+   }
+}
+
+
+.trainerImage{
+  padding-top: 30px;
+  width: 40px;
+  padding-bottom: 30px;
+}
+
+.winner-result{
+  background-color:#BDCAB7;
+}
+
+.loser-result{
+  background-color:#C7A1A1;
+}
 
 </style>
