242e3571e3eb557ac524e3020eed60944dbcd71a
danielavdalian <avdaliansfdk@gmail.com>
2023-03-22 19:50:07 +0100

    Deleted external div and optimized center of items

--- src/components/Medallas.vue
+++ src/components/Medallas.vue
@@ -1,41 +1,35 @@
 <template>
   <div class="medallas panel-body col">
     <div class="medalla">
-      <div class="medalla_2">
-        <img
-          v-if="ladoMedalla == 'L'"
-          src="@/assets/Medallas/Medalla_Céfiro.png"
-        />
-        <img v-else src="@/assets/Medallas/Medalla_Tormenta.png" />
-      </div>
+      <img
+        v-if="ladoMedalla == 'L'"
+        src="@/assets/Medallas/Medalla_Céfiro.png"
+      />
+      <img v-else src="@/assets/Medallas/Medalla_Tormenta.png" />
     </div>
+
     <div class="medalla">
-        <div class="medalla_2">
       <img
         v-if="ladoMedalla == 'L'"
         src="@/assets/Medallas/Medalla_Colmena.png"
       />
       <img v-else src="@/assets/Medallas/Medalla_Mineral.png" />
     </div>
-</div>
     <div class="medalla">
-        <div class="medalla_2">
       <img
         v-if="ladoMedalla == 'L'"
         src="@/assets/Medallas/Medalla_Planicie.png"
       />
       <img v-else src="@/assets/Medallas/Medalla_Glaciar.png" />
     </div>
-</div>
+
     <div class="medalla">
-        <div class="medalla_2">
       <img
         v-if="ladoMedalla == 'L'"
         src="@/assets/Medallas/Medalla_Niebla.png"
       />
       <img v-else src="@/assets/Medallas/Medalla_Dragón.png" />
     </div>
-</div>
   </div>
 </template>
 
@@ -52,38 +46,30 @@ export default {
 .medallas {
   background-color: #d9d9d9;
   border-radius: 15px;
-  height: 100%;
   margin-left: 5%;
   margin-right: 5%;
   border: 1px solid;
   border-color: grey;
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 }
 .medalla {
-  background-color: #888686;
-  border: 1px solid;
-  border-color: grey;
-  border-radius: 15px;  
+  background-color: #aaaaaa;
+  border: 5px solid;
+  border-color: #888686;
+  border-radius: 15px;
   width: 100%;
-  max-width:100px;
+  max-width: 100px;
   height: 100px;
   position: relative;
-  margin: 0 auto;
-  margin-top: 2%;
-  margin-bottom: 2%;
-}
-.medalla_2 {
-  background-color: #AAAAAA;
-  border: 1px solid;
-  border-color: grey;
-  width: 95%;
-  border-radius: 15px;  
-  height: 95%;
-  position: absolute;
-  top: 1.5%;
-  left: 1.5%;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 10%;
+  margin-top: 10%;
 }
+
 </style>
