5ac8b55c07ee0f8ba384b6ba088d0b139cb3ba8d
danielavdalian <avdaliansfdk@gmail.com>
2023-03-24 20:37:20 +0100

    Added profile selector (under construction)

---
+++ src/components/Selector.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="perfil">
+    <div class="row">
+      <div class="carrusel">
+        <div class="container-image">
+          <img class="img-carrusel" src="@/assets/skins/azul.png" />
+        </div>
+        <div class="container-image carrusel-selected">
+          <img class="img-carrusel" src="@/assets/skins/azul.png" />
+        </div>
+        <div class="container-image">
+          <img class="img-carrusel" src="@/assets/skins/azul.png" />
+        </div>
+      </div>
+    </div>
+      <div class="container-selected">
+        a
+        <img class="img-selected" src="@/assets/skins/verde.png" />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+.perfil {
+  border: 1px, solid, black;
+}
+.carrusel {
+  border: 1px, solid, black;
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
+}
+.carrusel-selected {
+  border: 3px solid;
+  border-radius: 20px;
+  border-color: #454545;
+}
+.container-image {
+  width: fit-content;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #c2bdbd;
+  margin-right: 5%;
+  border-radius: 15px;
+}
+.container-image:last-child {
+  margin-right: 0%;
+}
+.img-carrusel {
+  margin: 0 10px;
+  height: 70%;
+  width: 70%;
+}
+.container-selected {
+  
+  height: fit-content;
+  border: 200px, solid, black;
+}
+.img-selected {
+  height: 10%;
+}
+</style>
--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,13 +1,76 @@
 <template>
-  <h1>Perfil</h1>
+  <div class="mainCenter">
+    <p class="tituloFicha pl-title text-center border border-dark">
+      FICHA ENTRENADOR
+    </p>
+    <p class="nombreFicha pl-title text-center border border-dark">Guzzom</p>
+  </div>
+  <div class="row">
+    <div class="border border-5 col-sm-2">
+      <!--A BORRAR-->
+      <div class="medallas"></div>
+      <!--A BORRAR-->
+    </div>
+    <div class="border border-5 col-sm-8">
+      <div class="row">
+        <div class="container-perfil">
+          <Selector />
+        </div>
+      </div>
+      <div class="row"></div>
+    </div>
+    <div class="border border-5 col-sm-2"></div>
+  </div>
 </template>
 
 <script>
+import Selector from "@/components/Selector.vue";
 export default {
-
-}
+  components: { Selector },
+};
 </script>
-
-<style>
-
+<!--A BORRAR MEDALLAS-->
+<style scoped>
+.container-perfil {
+  height: 100%;
+  width: 80%;
+}
+.medallas {
+  height: 500px;
+}
+.container {
+  width: 100%;
+  height: 100%;
+}
+.perfil {
+  border: 1px solid;
+  border-color: grey;
+  border-radius: 15px;
+  background-color: #d9d9d9;
+  margin-left: 5%;
+  margin-right: 5%;
+  height: 100%;
+}
+.tituloFicha {
+  border-radius: 20px;
+  width: 75%;
+  margin: 0 auto;
+  padding: 5x;
+  font-weight: bold;
+}
+.mainCenter {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+  width: fit-content;
+}
+.nombreFicha {
+  border-radius: 20px;
+  margin: 0 auto;
+  width: fit-content;
+  padding: 15px;
+  margin-top: 10px;
+}
+.row {
+}
 </style>
