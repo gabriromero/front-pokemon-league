31a744667b3b9eb5a4371d83899d0fe879d3a6d0
GabriMac <gabri1998gabri@gmail.com>
2023-03-25 02:02:51 +0100

    añadidos archivos nuevos

---
+++ src/components/ZonasCaptura.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p class="title text-center">ZONAS DE CAPTURA</p>
+    
+    <div class="divBackground">
+        <div class="insideDiv">
+            <img id="map" class="img-fluid p-3 w-100" src="@/assets/mapa.png" alt="">
+        </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.divBackground {
+  background-color: #C2BDBD;
+  border: 2px solid;
+  border-radius: 15px 15px 15px 15px;  
+  margin-bottom: 5%;
+  
+}
+
+.insideDiv {
+  background-color: white;
+  border: 2px solid;
+  border-radius: 15px 15px 15px 15px;
+  height: auto;
+  margin: 5px;
+  vertical-align: middle;
+}
+
+.title{
+    font-size: 12px;
+    margin-top: 15px;
+}
+
+</style>
