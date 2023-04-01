bf6bbf0525460dc068adc54f3f0aa2842694d42e
TheJohniX <thejohnix10@gmail.com>
2023-04-01 23:36:58 +0200

    LO-4-nutrir-combate-backend

--- src/helpers/generalHelper.js
+++ src/helpers/generalHelper.js
@@ -17,6 +17,6 @@ export function getJornada() {
     return posicionJornada === -1 ? -1 : posicionJornada + 1;
 }
 
-export const API_URL = 'https://pokemon-league-api.herokuapp.com'
+export const API_URL = 'http://127.0.0.1:5000'
 export const API_URL_LOCAL = 'http://127.0.0.1:5000'
 export const API_PKM = 'https://pokeapi.co/api/v2/pokemon'
--- src/views/CombateView.vue
+++ src/views/CombateView.vue
@@ -1,76 +1,139 @@
 <template>
   <div id="root">
-    <img class="img-background" src="@/assets/pokeball_fondo.svg" draggable="false">
+    <img
+      class="img-background"
+      src="@/assets/pokeball_fondo.svg"
+      draggable="false"
+    />
     <div id="combate" class="pt-2 container">
       <div>
         <h3>COMBATES PENDIENTES</h3>
         <div class="conjunto-combates">
-
           <div class="mt-4"></div>
 
-
-          <div v-for="(myMatch, index) in myMatches" :key="index" class="border-single-match">
+          <div
+            v-for="(myMatch, index) in myMatches"
+            :key="index"
+            class="border-single-match"
+          >
             <div class="single-match container-fluid">
-              <div class="row d-flex align-items-center" v-if="!confirmResults[index] && !confirmedResults[index]">
-                <div :id="'player-1-' + index" class="col-lg-5 trainer-select" v-on:click="confirmResults[index] = !confirmResults[index]; obtainPlayerName1(index)">
-                  <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" /> {{myMatch.player_1_username}}
+              <div
+                class="row d-flex align-items-center"
+                v-if="!confirmResults[index] && !confirmedResults[index]"
+              >
+                <div
+                  :id="'player-1-' + index"
+                  class="col-lg-5 trainer-select"
+                  v-on:click="
+                    confirmResults[index] = !confirmResults[index];
+                    obtainPlayerName1(index);
+                  "
+                >
+                  <img
+                    class="mt-1 mb-1 trainerImage"
+                    src="@/assets/trainerPixel.png"
+                    draggable="false"
+                  />
+                  {{ myMatch.player_1_username }}
                 </div>
-                <div :id="'player-2-' + index" class="col-lg-5 trainer-select " v-on:click="confirmResults[index] = !confirmResults[index]; obtainPlayerName2(index)">
-                  {{myMatch.player_2_username}} <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" />
-
-
+                <div
+                  :id="'player-2-' + index"
+                  class="col-lg-5 trainer-select"
+                  v-on:click="
+                    confirmResults[index] = !confirmResults[index];
+                    obtainPlayerName2(index);
+                  "
+                >
+                  {{ myMatch.player_2_username }}
+                  <img
+                    class="mt-1 mb-1 trainerImage"
+                    src="@/assets/trainerPixel.png"
+                    draggable="false"
+                  />
                 </div>
                 <div class="col-lg-2 handicap">
                   <div class="handicap-div">
-                    {{myMatch.diferencia}}
+                    {{ myMatch.diferencia }}
                   </div>
                 </div>
               </div>
 
-              <div class="row d-flex justify-content-center align-items-center"
-                v-else-if="confirmResults[index] && !confirmedResults[index]">
-                <div v-on:click="confirmResults[index] = !confirmResults[index]"
-                  class="col-lg-2 trainer-select confirming-result confirm-no d-flex justify-content-center align-items-center">
+              <div
+                class="row d-flex justify-content-center align-items-center"
+                v-else-if="confirmResults[index] && !confirmedResults[index]"
+              >
+                <div
+                  v-on:click="confirmResults[index] = !confirmResults[index]"
+                  class="col-lg-2 trainer-select confirming-result confirm-no d-flex justify-content-center align-items-center"
+                >
                   No
                 </div>
-                <div id="confirm-message"
-                  class="col-lg-8 trainer-select confirming-result d-flex justify-content-center align-items-center">
-                  ¿ {{nombrePlayerGanador[index]}} ha ganado ?
+                <div
+                  id="confirm-message"
+                  class="col-lg-8 trainer-select confirming-result d-flex justify-content-center align-items-center"
+                >
+                  ¿ {{ nombrePlayerGanador[index] }} ha ganado ?
                 </div>
                 <div
                   class="col-lg-2 trainer-select confirming-result confirm-yes d-flex justify-content-center align-items-center"
-                  v-on:click="confirmedResults[index] = !confirmedResults[index]">
+                  v-on:click="
+                    confirmedResults[index] = !confirmedResults[index]
+                  "
+                >
                   Si
                 </div>
               </div>
 
-              <div class="row d-flex justify-content-center align-items-center border border-dark" v-else>
-                <div 
-                  :class="' '+myMatch.player_1_username === nombrePlayerGanador[index] ? 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center winner-text' : 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center loser-text'">
-                  <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" />&nbsp;{{myMatch.player_1_username}}
+              <div
+                class="row d-flex justify-content-center align-items-center border border-dark"
+                v-else
+              >
+                <div
+                  :class="
+                    ' ' + myMatch.player_1_username ===
+                    nombrePlayerGanador[index]
+                      ? 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center winner-text'
+                      : 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center loser-text'
+                  "
+                >
+                  <img
+                    class="mt-1 mb-1 trainerImage"
+                    src="@/assets/trainerPixel.png"
+                    draggable="false"
+                  />&nbsp;{{ myMatch.player_1_username }}
                 </div>
                 <div
-                  class="col-lg-1 waiting-confirmation confirming-result d-flex justify-content-center align-items-center">
+                  class="col-lg-1 waiting-confirmation confirming-result d-flex justify-content-center align-items-center"
+                >
                   VS
-                </div>                
-                <div
-                :class="myMatch.player_2_username+' ' === nombrePlayerGanador[index] ? 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center winner-text' : 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center loser-text'">
-                  {{myMatch.player_2_username}}&nbsp;<img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" />
                 </div>
                 <div
-                  class="col-lg-3 waiting-confirmation confirming-result d-flex justify-content-center align-items-center">
+                  :class="
+                    myMatch.player_2_username + ' ' ===
+                    nombrePlayerGanador[index]
+                      ? 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center winner-text'
+                      : 'col-lg-4 waiting-confirmation confirming-result d-flex justify-content-center align-items-center loser-text'
+                  "
+                >
+                  {{ myMatch.player_2_username }}&nbsp;<img
+                    class="mt-1 mb-1 trainerImage"
+                    src="@/assets/trainerPixel.png"
+                    draggable="false"
+                  />
+                </div>
+                <div
+                  class="col-lg-3 waiting-confirmation confirming-result d-flex justify-content-center align-items-center"
+                >
                   Esperando ...
                 </div>
               </div>
-
             </div>
-          </div> 
+          </div>
           <div class="mb-4"></div>
         </div>
       </div>
       <div class="mt-4">
-
-        <br><br>
+        <br /><br />
         <h3>COMBATES FINALIZADOS</h3>
 
         <div class="conjunto-combates">
@@ -80,15 +143,23 @@
               <div class="trainer-select-finished loser-result">
                 <div class="row d-flex align-items-center">
                   <div class="col-lg-4">
-                    <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" /> Avdalian
+                    <img
+                      class="mt-1 mb-1 trainerImage"
+                      src="@/assets/trainerPixel.png"
+                      draggable="false"
+                    />
+                    Avdalian
                   </div>
-                  <div class="col-lg-1">
-                    VS
-                  </div>
+                  <div class="col-lg-1">VS</div>
                   <div class="col-lg-4">
-                    <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" /> Avdalian
+                    <img
+                      class="mt-1 mb-1 trainerImage"
+                      src="@/assets/trainerPixel.png"
+                      draggable="false"
+                    />
+                    Avdalian
                   </div>
-                  <div id="winner" class="col-lg-3 ">
+                  <div id="winner" class="col-lg-3">
                     <h3><b>GANA GUZZOM</b></h3>
                   </div>
                 </div>
@@ -102,15 +173,23 @@
               <div class="trainer-select-finished winner-result">
                 <div class="row d-flex align-items-center">
                   <div class="col-lg-4">
-                    <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" /> Avdalian
+                    <img
+                      class="mt-1 mb-1 trainerImage"
+                      src="@/assets/trainerPixel.png"
+                      draggable="false"
+                    />
+                    Avdalian
                   </div>
-                  <div class="col-lg-1">
-                    VS
-                  </div>
+                  <div class="col-lg-1">VS</div>
                   <div class="col-lg-4">
-                    <img class="mt-1 mb-1 trainerImage" src="@/assets/trainerPixel.png" draggable="false" /> Avdalian
+                    <img
+                      class="mt-1 mb-1 trainerImage"
+                      src="@/assets/trainerPixel.png"
+                      draggable="false"
+                    />
+                    Avdalian
                   </div>
-                  <div id="winner" class="col-lg-3 ">
+                  <div id="winner" class="col-lg-3">
                     <h3><b>GANA GUZZOM</b></h3>
                   </div>
                 </div>
@@ -127,51 +206,48 @@
 </template>
 
 <script>
-import {getMyMatches} from '@/api/combate'
+import { getMyMatches } from "@/api/combate";
 export default {
   data() {
     return {
-
-
       confirmResults: [],
       confirmedResults: [],
-      myMatches: '',      
-      nombrePlayerGanador: []
-
-    }
+      myMatches: "",
+      nombrePlayerGanador: [],
+    };
   },
   methods: {
     async fillMyMatches() {
-      const accessToken = localStorage.getItem('access_token');
-      let myMatches = await getMyMatches(accessToken)
+      const accessToken = localStorage.getItem("access_token");
+      let myMatches = await getMyMatches(accessToken);
 
-      this.myMatches = myMatches.data      
+      this.myMatches = myMatches.data;
     },
 
-    obtainPlayerName1(index){
-      const player_1_username = document.getElementById("player-1-" + index).textContent;            
-      this.nombrePlayerGanador[index] = player_1_username;  
-      
+    obtainPlayerName1(index) {
+      const player_1_username = document.getElementById(
+        "player-1-" + index
+      ).textContent;
+      this.nombrePlayerGanador[index] = player_1_username;
+
       console.log(this.nombrePlayerGanador[index]);
       this.confirmResults[index] = true;
       this.confirmedResults[index] = false;
-     
     },
-    obtainPlayerName2(index){
-      const player_2_username = document.getElementById("player-2-" + index).textContent;             
-      this.nombrePlayerGanador[index] = player_2_username;      
-      console.log(this.nombrePlayerGanador[index]);  
+    obtainPlayerName2(index) {
+      const player_2_username = document.getElementById(
+        "player-2-" + index
+      ).textContent;
+      this.nombrePlayerGanador[index] = player_2_username;
+      console.log(this.nombrePlayerGanador[index]);
       this.confirmResults[index] = true;
-      this.confirmedResults[index] = false;   
-    
-
-    }
+      this.confirmedResults[index] = false;
+    },
   },
   mounted() {
-    this.fillMyMatches()
+    this.fillMyMatches();
   },
-
-}
+};
 </script>
 
 <style scoped>
@@ -182,17 +258,15 @@ export default {
   overflow: hidden;
 }
 
-
 #combate {
   text-align: center;
-
 }
 
 .conjunto-combates {
   border: solid #000000;
   border-width: 1px;
   border-radius: 15px;
-  background-color: #D0D0D0;
+  background-color: #d0d0d0;
 }
 
 .border-single-match {
@@ -203,31 +277,31 @@ export default {
 }
 
 .single-match {
-  border: solid #AAAAAA;
+  border: solid #aaaaaa;
   border-radius: 15px;
   border-width: 10px;
-  background-color: #AAAAAA;
+  background-color: #aaaaaa;
 }
 
 .trainer-select {
   border: solid black;
   border-width: 1px;
-  background-color: #D9D9D9;
+  background-color: #d9d9d9;
 }
 
 .waiting-confirmation {
-  background-color: #D9D9D9;
+  background-color: #d9d9d9;
 }
 
 .trainer-select-finished {
   border: solid black;
   border-width: 1px;
-  background-color: #D9D9D9;
+  background-color: #d9d9d9;
   border-radius: 7px;
 }
 
 .trainer-select:hover:not(.confirming-result) {
-  background-color: #BDCAB7;
+  background-color: #bdcab7;
   cursor: pointer;
   -moz-user-select: none;
   -khtml-user-select: none;
@@ -237,7 +311,7 @@ export default {
 }
 
 .trainer-select-red {
-  background-color: #BDCAB7;
+  background-color: #bdcab7;
   cursor: pointer;
   -moz-user-select: none;
   -khtml-user-select: none;
@@ -250,7 +324,7 @@ export default {
   border: solid black;
   border-width: 1px;
   border-radius: 15px;
-  background-color: #D9D9D9;
+  background-color: #d9d9d9;
   font-size: 12px;
   height: 96px;
   margin: auto;
@@ -301,11 +375,11 @@ export default {
 }
 
 .winner-result {
-  background-color: #BDCAB7;
+  background-color: #bdcab7;
 }
 
 .loser-result {
-  background-color: #C7A1A1;
+  background-color: #c7a1a1;
 }
 
 .confirming-result {
@@ -313,7 +387,7 @@ export default {
 }
 
 .confirm-no {
-  background-color: #C7A1A1;
+  background-color: #c7a1a1;
 }
 
 .confirm-no:hover {
@@ -327,8 +401,7 @@ export default {
 }
 
 .confirm-yes {
-  background-color: #BDCAB7;
-
+  background-color: #bdcab7;
 }
 
 .confirm-yes:hover {
@@ -347,4 +420,5 @@ export default {
 
 .loser-text {
   color: rgb(145, 24, 24);
-}</style>
+}
+</style>
