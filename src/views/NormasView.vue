3e4bbf00b9c21d02a727c2de0fa3d97edee3f726
GabriMac <gabri1998gabri@gmail.com>
2023-04-06 04:22:25 +0200

    handicaps acabados

--- src/views/NormasView.vue
+++ src/views/NormasView.vue
@@ -5,7 +5,7 @@
       <div class="main">
         <div class="row mt-3 ">
           <button class="col-sm-4 secciones" v-on:click="numSeccion = 1">Mercado negro</button>
-          <button class="col-sm-4 secciones" v-on:click="numSeccion = 2">Límite Stats/EVs</button>
+          <button class="col-sm-4 secciones" v-on:click="numSeccion = 2">Límites y niveles</button>
           <button class="col-sm-4 secciones" v-on:click="numSeccion = 3">Cambios y crianza</button>
         </div>
 
@@ -16,7 +16,7 @@
         </div>
 
         <div class="border-example framed mt-4 mb-5 padding-sides" style="background-color: #9eacad;">
-          <div v-if="numSeccion == 1">
+          <div class="small" v-if="numSeccion == 1">
             <h4><b><u>Mercado negro</u></b></h4>
             <div class="mt-3">
               <p class="mb-4">El uso del Mercado Negro solo será permitido cuando se 
@@ -25,34 +25,295 @@
               <p>Queda totalmente prohibido la compra de absolutamente cualquier Pokémon.</p>
               <h6><b><u>Objetos</u></b></h6>
               <p><i>Está prohibida la compra de cualquier objeto a excepción de:</i></p>
-              <p>1. Todos los objetos que estén a la venta en Tienda Pokémon o Centro Comercial disponibles en las rutas determinadas en el mapa.</p>
-              <p>2. Carameloraros</p>
-              <p>3. Vitaminas</p>
-              <p>4. Bayas reductoras de EVs</p>
-              <p>5. Campana alivio</p>
+              <p>1. Carameloraros</p>
+              <p>2. Vitaminas</p>
+              <p>3. Bayas reductoras de EVs</p>
+              <p>4. Campana alivio</p>
+              <p>5. Escamas corazón</p>
+              <p>6. Todos los objetos que estén a la venta en Tienda Pokémon o Centro Comercial disponibles en las rutas determinadas en el mapa.</p>
+              <p>7. <u>En la última jornada</u> se pueden comprar objetos equipables competitivos sin restricción.</p>
             </div>
           </div>
 
           <div v-if="numSeccion == 2">
-            <h4><u>Sección 2</u></h4>
-            <div class="mt-3">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+
+            <h4 ><u>Niveles por Jornada</u></h4>
+
+            <div class="mt-3 small">
+              <p>Por cada Jornada, se establecerá una cantidad y niveles del equipo Pokémon.</p>
+              <div class="row text-center">
+                  <div class="col-3">
+                    <div><b><u>Jornada</u></b></div>
+                  </div>
+                  <div class="col-9">
+                    <div><b><u>Niveles</u></b></div>
+                  </div>
+                </div>
+
+                <div class="row mt-3 text-center">
+                  <div class="col-3">
+                    <div>1</div>
+                  </div>
+                  <div class="col-9">
+                    <div>11|12|13</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>2</div>
+                  </div>
+                  <div class="col-9">
+                    <div>15|16|17</div>
+                  </div>
+                </div>
+                
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>3</div>
+                  </div>
+                  <div class="col-9">
+                    <div>17|18|19</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>4</div>
+                  </div>
+                  <div class="col-9">
+                    <div>22|23|24|25</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>5</div>
+                  </div>
+                  <div class="col-9">
+                    <div>28|29|30|31</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>6</div>
+                  </div>
+                  <div class="col-9">
+                    <div>31|31|32|32|33</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>7</div>
+                  </div>
+                  <div class="col-9">
+                    <div>33|33|34|34|35</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>8</div>
+                  </div>
+                  <div class="col-9">
+                    <div>38|39|40|40|41|41</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-3">
+                    <div>9</div>
+                  </div>
+                  <div class="col-9">
+                    <div>50|50|50|50|50|50</div>
+                  </div>
+                </div>
             </div>
+            <h4 class="mt-5"><u>Límite Stats/EVs</u></h4>
+            <div class="mt-3">
+              <p>Por cada Jornada, se establecerá un límite de Stats y de EVs.</p>
+              <div class="row ">
+                  <div class="col-4">
+                    <div><b><u>Jor.</u></b></div>
+                  </div>
+                  <div class="col-4">
+                    <div><b><u>Stats</u></b></div>
+                  </div>
+                  <div class="col-4">
+                    <div><b><u>EVs</u></b></div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>1</div>
+                  </div>
+                  <div class="col-4">
+                    <div>389</div>
+                  </div>
+                  <div class="col-4">
+                    <div>30</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>2</div>
+                  </div>
+                  <div class="col-4">
+                    <div>419</div>
+                  </div>
+                  <div class="col-4">
+                    <div>60</div>
+                  </div>
+                </div>
+                
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>3</div>
+                  </div>
+                  <div class="col-4">
+                    <div>449</div>
+                  </div>
+                  <div class="col-4">
+                    <div>90</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>4</div>
+                  </div>
+                  <div class="col-4">
+                    <div>449</div>
+                  </div>
+                  <div class="col-4">
+                    <div>160</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>5</div>
+                  </div>
+                  <div class="col-4">
+                    <div>499</div>
+                  </div>
+                  <div class="col-4">
+                    <div>230</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>6</div>
+                  </div>
+                  <div class="col-4">
+                    <div>499</div>
+                  </div>
+                  <div class="col-4">
+                    <div>300</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>7</div>
+                  </div>
+                  <div class="col-4">
+                    <div>549</div>
+                  </div>
+                  <div class="col-4">
+                    <div>370</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>8</div>
+                  </div>
+                  <div class="col-4">
+                    <div>549</div>
+                  </div>
+                  <div class="col-4">
+                    <div>440</div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-4">
+                    <div>9</div>
+                  </div>
+                  <div class="col-4">
+                    <div>-</div>
+                  </div>
+                  <div class="col-4">
+                    <div>510</div>
+                  </div>
+                </div>
+            
+            </div>
+
+            
+            <h4 class="mt-5"><u>MTs</u></h4>
+            Se aplicarán restricciones por potencia a las MTs que puedas comprar.
+
+            <div class="row mt-3">
+              <div class="col-6">
+                <div><b><u>Jor.</u></b></div>
+              </div>
+              <div class="col-6">
+                <div><b><u>Potencia</u></b></div>
+              </div>
+            </div>
+
+            <div class="row mt-3">
+              <div class="col-6">
+                <div>1-5</div>
+              </div>
+              <div class="col-6">
+                <div>70</div>
+              </div>
+            </div>
+
+            <div class="row mt-3">
+              <div class="col-6">
+                <div>6-7</div>
+              </div>
+              <div class="col-6">
+                <div>90</div>
+              </div>
+            </div>
+
+            
+
+            <div class="row mt-3">
+              <div class="col-6">
+                <div>8-9</div>
+              </div>
+              <div class="col-6">
+                <div>-</div>
+              </div>
+            </div>
+
           </div>
 
-          <div v-if="numSeccion == 3">
+          <div class="small" v-if="numSeccion == 3">
             <h4><b><u>Cambios y crianza</u></b></h4>
             <div class="mt-3">
               <h6 class="mt-3"><b><u>Cambios</u></b></h6>
               <p>Solo se permitirá hacer un cambio de Pokémon en el equipo respecto al equipo utilizado en la jornada anterior.</p>
               <p>Se podrá capturar Pokémon salvaje de la misma especie que su Pokémon inicial para evitar tener un inicial neutro.</p>
+              <p>Solo en la Jornada 9, la Jornada final, se podrá capturar únicamente un Pokémon semilegendario (600 Stats).</p>
               <h6 class="mt-5"><b><u>Crianza</u></b></h6>
               <p>La crianza estará disponible a partir de la <b>Jornada 6</b>.</p>
               <p>Los Pokémon conseguidos a través de crianza solo podrán tener como máximo 2 stats con los IVs al 31.</p>
             </div>
           </div>
 
-          <div v-if="numSeccion == 4" >
+          <div class="small" v-if="numSeccion == 4" >
             <h4><b> <u>Capturas de pantalla</u></b></h4>
             <div class="mt-3">
               <p style="">Al contrario que en otras ediciones, en esta no es obligatorio hacer captura de pantalla para validar tu equipo antes de un combate.</p><br>
@@ -62,17 +323,108 @@
           </div>
 
           <div v-if="numSeccion == 5">
-            <h4><u>Sección 5</u></h4>
-            <div class="mt-3">
-              On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.
+            <h4><u>Handicaps</u></h4>
+            <div class="mt-3 small">
+              <p>No queremos que los jugadores más experimentados se aburran en el torneo aplastando a los novatos, por eso, se han incluido los handicaps, para que puedan tener un poco de desafío y no pierdan el interés antes de la última jornada.</p>
+              <p>Además, quién sabe, quizás algún novato sorprenda a todos y se convierta en el campeón del torneo.</p>
+
+              <p class="mt-5"><i>Los handicaps son acumulativos</i></p>
+              <div class="small mt-2">
+                <div class="row ">
+                  <div class="col-6">
+                    <div><b><u>Diferencia</u></b></div>
+                  </div>
+                  <div class="col-6">
+                    <div><b><u>Handicap</u></b></div>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>3</p>
+                  </div>
+                  <div class="col-6">
+                    <p>1 Pokémon menos</p>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>4</p>
+                  </div>
+                  <div class="col-6">
+                    <p>Enseñar el equipo al rival</p>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>5</p>
+                  </div>
+                  <div class="col-6">
+                    <p>No equipar objetos</p>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>6</p>
+                  </div>
+                  <div class="col-6">
+                    <p>Intercambiar un Pokémon (sin objetos)</p>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>7</p>
+                  </div>
+                  <div class="col-6">
+                    <p>Te eligen el Pokémon que iniciará el combate</p>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>8</p>
+                  </div>
+                  <div class="col-6">
+                    <p>1 Pokémon menos</p>
+                  </div>
+                </div>
+
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>9</p>
+                  </div>
+                  <div class="col-6">
+                    <p>Cambios no permitidos en combate</p>
+                  </div>
+                </div>
+                
+                <div class="row mt-3">
+                  <div class="col-6">
+                    <p>10</p>
+                  </div>
+                  <div class="col-6">
+                    <p>Rival recibe x3 si gana</p>
+                  </div>
+                </div>
+              </div>
+
+
             </div>
+
+            <div class="mt-5"><i><p>El handicap de 10 de diferencia no se aplicará en la <u>última Jornada</u>.</p>
+                              Los puntos en la última Jornada <u>serán dobles</u>.</i></div>
           </div>
 
-          <div v-if="numSeccion == 6">
+          <div class="small" v-if="numSeccion == 6">
             <h4><b><u>Objetos y capturas</u></b></h4>
             <div class="mt-3">
               <p><b>Equipar</b> objetos para el combate estará permitido a partir de la <b>Jornada 4</b>.</p>
-              <p>Tanto la obtención de objetos como la captura de Pokémon, se realizará exclusivamente en las rutas determinadas en el mapa de cada jornada.</p>
+              <p class="mt-5">Tanto la obtención de objetos como la captura de Pokémon, se realizará exclusivamente en las rutas determinadas en el mapa de cada jornada.</p>
+              <p class="mt-5">No se puede equipar el mismo objeto en varios Pokémon.</p>
             </div>
           </div>
         </div>
@@ -84,11 +436,10 @@
 </template>
 
 <script>
-import {getFakePlayers} from '@/api/home'
 export default {
   data() {
     return{
-      numSeccion: 3,
+      numSeccion: 2,
     }
   },
 }
@@ -111,6 +462,14 @@ export default {
 .padding-sides{
   padding-left: 10%;
   padding-right: 10%;
+}
+
+
+
+@media screen and (max-width: 600px){
+  .small{
+    font-size: 12px;
+  }
 }
 
 
