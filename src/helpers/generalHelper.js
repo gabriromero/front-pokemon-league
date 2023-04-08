0a65a02dc046dd56007703f13953a9f3894e056e
GabriMac <gabri1998gabri@gmail.com>
2023-04-08 03:03:03 +0200

    normas kanto

--- src/helpers/generalHelper.js
+++ src/helpers/generalHelper.js
@@ -16,14 +16,30 @@ export const evsMaximos = {
 }
 
 export const statsMaximos = {
+    1:389, 2:419, 3:449, 4:499, 
+    5:499, 6:559, 7:559, 8:559, 9:"MAX"
+}
+
+/*
+JOHTO:
+export const statsMaximos = {
     1:389, 2:419, 3:449, 4:449, 
     5:499, 6:499, 7:549, 8:549, 9:"MAX"
 }
+*/
 
+export const nivelesPokemon = {
+    1: [12,13,14], 2: [19,20,21], 3: [22,23,24], 4: [26,27,28,29], 
+    5: [40,41,42,43], 6: [42,43,43,44,45], 7: [45,46,46,47,47], 8: [48,48,49,49,50,50], 9: [50,50,50,50,50,50]
+}
+
+/*
+JOHTO:
 export const nivelesPokemon = {
     1: [11,12,13], 2: [15,16,17], 3: [17,18,19], 4: [22,23,24,25], 
     5: [28,29,30,31], 6: [31,31,32,32,33], 7: [33,33,34,34,35], 8: [38,39,40,40,41,41], 9: [50,50,50,50,50,50]
 }
+*/
 
 export const potenciaMt = {
     "1-5": 70,
@@ -31,6 +47,27 @@ export const potenciaMt = {
     "8-9": "MAX",
 }
 
+const nuevasRutas = {
+    1: [["Pueblo Paleta","https://www.wikidex.net/wiki/Pueblo_Paleta"],["Ruta 1","https://www.wikidex.net/wiki/Ruta_1_(Kanto)"],["Ciudad Verde","https://www.wikidex.net/wiki/Ciudad_Verde"],
+        ["Ruta 22","https://www.wikidex.net/wiki/Ruta_22_(Kanto)"],["Ruta 2","https://www.wikidex.net/wiki/Ruta_2_(Kanto)"],["Bosque Verde","https://www.wikidex.net/wiki/Bosque_verde"],
+        ["Ciudad Plateada","https://www.wikidex.net/wiki/Ciudad_plateada"]], 
+    2: [["Ruta 3","https://www.wikidex.net/wiki/Ruta_3_(Kanto)"],["Monte Moon","https://www.wikidex.net/wiki/Monte_moon"],["Ruta 4","https://www.wikidex.net/wiki/Ruta_4_(Kanto)"],["Ciudad celeste","https://www.wikidex.net/wiki/Ciudad_celeste"],
+        ["Ruta 24","https://www.wikidex.net/wiki/Ruta_24_(Kanto)"],["Ruta 25","https://www.wikidex.net/wiki/Ruta_25_(Kanto)"]],
+    3: [["Ruta 5","https://www.wikidex.net/wiki/Ruta_5_(Kanto)"],["Ruta 6","https://www.wikidex.net/wiki/Ruta_6_(Kanto)"],["Ciudad Carmín","https://www.wikidex.net/wiki/Ciudad_Carmín"],["Ruta 11","https://www.wikidex.net/wiki/Ruta_11_(Kanto)"],["Cueva Diglett","https://www.wikidex.net/wiki/Cueva_diglett"]], 
+    4: [["Ruta 9","https://www.wikidex.net/wiki/Ruta_9_(Kanto)"],["Ruta 10","https://www.wikidex.net/wiki/Ruta_10_(Kanto)"],["Túnel roca","https://www.wikidex.net/wiki/tunel_roca"],["Pueblo Lavanda","https://www.wikidex.net/wiki/Pueblo_lavanda"],
+        ["Ruta 8","https://www.wikidex.net/wiki/Ruta_8_(Kanto)"],["Ruta 7","https://www.wikidex.net/wiki/Ruta_7_(Kanto)"],["Ciudad Azulona","https://www.wikidex.net/wiki/Ciudad_Azulona"]], 
+    5: [["Torre Pokémon","https://www.wikidex.net/wiki/torre_pokemon"],["Ruta 12","https://www.wikidex.net/wiki/Ruta_12_(Kanto)"],["Ruta 13","https://www.wikidex.net/wiki/Ruta_13_(Kanto)"],["Ruta 14","https://www.wikidex.net/wiki/Ruta_14_(Kanto)"],
+        ["Ruta 15","https://www.wikidex.net/wiki/Ruta_15_(Kanto)"],["Ruta 18","https://www.wikidex.net/wiki/Ruta_18_(Kanto)"],["Ruta 17","https://www.wikidex.net/wiki/Ruta_17_(Kanto)"],["Ruta 16","https://www.wikidex.net/wiki/Ruta_16_(Kanto)"]],
+    6: [["Ciudad Azafrán","https://www.wikidex.net/wiki/Ciudad_azafran"]],
+    7: [["Ruta 21","https://www.wikidex.net/wiki/Ruta_21_(Kanto)"],["Isla Canela","https://www.wikidex.net/wiki/Isla_canela"],["Mansión Pokémon","https://www.wikidex.net/wiki/Mansion_Pokemon"]],
+    8: [["Isla Prima","https://www.wikidex.net/wiki/Isla_prima"],["Camino Candente","https://www.wikidex.net/wiki/Camino_candente"],["Playa Tesoro","https://www.wikidex.net/wiki/Playa_tesoro"],["Monte Ascuas","https://www.wikidex.net/wiki/Monte_ascuas"],
+        ["Isla Secunda","https://www.wikidex.net/wiki/Isla_secunda"],["Cabo extremo","https://www.wikidex.net/wiki/Cabo_extremo"],["Isla Tera","https://www.wikidex.net/wiki/Isla_tera"],["Puente Unión","https://www.wikidex.net/wiki/Puente_union"],
+        ["Bosque Baya","https://www.wikidex.net/wiki/Bosque_baya"],["Ruta 19","https://www.wikidex.net/wiki/Ruta_19_(Kanto)"],["Ruta 20","https://www.wikidex.net/wiki/Ruta_20_(Kanto)"],["Islas Espuma","https://www.wikidex.net/wiki/Islas_espuma"],["Central Energía","https://www.wikidex.net/wiki/Central_energia"]],
+    9: [["Ruta 23","https://www.wikidex.net/wiki/Ruta_23_(Kanto)"],["Calle Victoria","https://www.wikidex.net/wiki/Calle_Victoria_(Kanto)"]],
+}
+
+/*
+JOHTO:
 const nuevasRutas = {
     1: [["Pueblo Primavera","https://www.wikidex.net/wiki/Pueblo_Primavera"],["Ruta 29","https://www.wikidex.net/wiki/Ruta_29"],["Ruta 46","https://www.wikidex.net/wiki/Ruta_46"],
         ["Ciudad Cerezo","https://www.wikidex.net/wiki/Ciudad_Cerezo"],["Ruta 30","https://www.wikidex.net/wiki/Ruta_30"],["Ruta 31","https://www.wikidex.net/wiki/Ruta_31"],
@@ -49,6 +86,7 @@ const nuevasRutas = {
     9: [["Guarida Dragón","https://www.wikidex.net/wiki/Guarida_Drag%C3%B3n"],["Ruta 27","https://www.wikidex.net/wiki/ruta_27"],["Ruta 26","https://www.wikidex.net/wiki/Ruta_26"],["Cataratas Tohjo","https://www.wikidex.net/wiki/Cataratas_Tohjo"],
         ["Calle Victoria","https://www.wikidex.net/wiki/Calle_Victoria_(Kanto)"],["Monte Plateado","https://www.wikidex.net/wiki/Monte_Plateado"]],
 }
+*/
 
 const handicaps = {
 	"3": "1 Pkm menos",
