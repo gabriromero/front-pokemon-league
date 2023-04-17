79f72cba6edcf50968f2c51dcc4d4a8a58ec0bcb
GabriMac <gabri1998gabri@gmail.com>
2023-04-18 01:58:26 +0200

    ejemplo implementacion carrusel

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -2,35 +2,74 @@
   <div class="perfil">
     <div class="carrusel">
       <div class="arrow">
-        <img class="img-arrow" src="@/assets/arrow.png" />
+        <img class="img-arrow" v-on:click="changeLeft" src="@/assets/arrow.png" />
       </div>
       <div class="container-image">
-        <img class="img-carrusel" src="@/assets/skins/azul.png" />
+        <img class="img-carrusel" :src="require(`@/assets/skins/trainer-${left}.png`)" />
       </div>
       <div class="container-image carrusel-selected">
-        <img class="img-carrusel" src="@/assets/skins/azul.png" />
+        <img class="img-carrusel" :src="require(`@/assets/skins/trainer-${middle}.png`)" />
       </div>
       <div class="container-image last">
-        <img class="img-carrusel" src="@/assets/skins/azul.png" />
+        <img class="img-carrusel" :src="require(`@/assets/skins/trainer-${right}.png`)" />
       </div>
       <div class="arrow">
-        <img class="img-arrow" src="@/assets/arrow.png" />
+        <img class="img-arrow"  v-on:click="changeRight" src="@/assets/arrow.png" />
       </div>
     </div>
     <div class="container-selected">
-      <img class="img-selected" src="@/assets/skins/verde.png" />
+      <img class="img-selected" :src="require(`@/assets/skins/trainer-${selected}.png`)" />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Selector",
+  data() {
+    return {
+      selected: 2,
+      left : 1,
+      middle : 2,
+      right : 3,
+      selectedImage: "@/assets/skins/verde.png",
+    };
+  },
+  methods: {
+    changeLeft() {
+      debugger;
+      this.left = this.anterior(this.left);
+      this.middle = this.anterior(this.middle);
+      this.right = this.anterior(this.right);
+      this.selected = this.middle;
+    },
+    changeRight() {
+      this.left = this.siguiente(this.left);
+      this.middle = this.siguiente(this.middle);
+      this.right = this.siguiente(this.right);
+      this.selected = this.middle;
+    },
+    anterior(value) {
+      value = value - 1
+      if(value == 0){
+        value = 30
+      }
+      return value
+    },
+    siguiente(value) {
+      value = value + 1
+      if(value == 31){
+        value = 1
+      }
+      return value
+    },
+  },
+};
 </script>
 
 <style scoped>
 .perfil {
   border: 1px, solid, black;
-  width: 100%;
   margin-left: 0px !important;
   margin-right: 0px !important; 
 }
@@ -52,16 +91,30 @@ row {
     width: 70%;
   }
 }
+@media (max-width: 450px){
+  .img-arrow {
+    width: 50% !important;
+  }
+  .container-image{
+    width: 120% !important;
+  }
+}
+@media (max-width: 400px){
+  .img-arrow {
+    width: 50% !important;
+  }
+  .container-image{
+    width: 150% !important;
+  }
+}
 .arrow:last-child {
   transform: rotate(180deg);
 }
 .carrusel {
-  border: 1px, solid, black;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 10%;
-  height: 100%;
 }
 .carrusel-selected {
   border: 3px solid;
@@ -85,7 +138,7 @@ row {
   margin: 0 10px;
   height: 70%;
   padding: 10%;
-  width: 100%;
+  width: 70%;
 }
 .container-selected {
   height: fit-content;
@@ -100,7 +153,7 @@ row {
 }
 .img-selected {
   height: 30%;
-  width: 100%;
+  width: 90%;
   padding: 10%;
   overflow: hidden;
 }
